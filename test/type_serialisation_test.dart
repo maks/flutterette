e70b8f37ff4d48258ba87f7977d8bfe96b349c17
Maksim Lin <maks@manichord.com>
2020-02-14 13:24:19 +1100

    add tests for widget type json deserialisation

---
+++ test/type_serialisation_test.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flutterette/widget_type.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('deserialise widget types', () {
+    test('deserialise Page empty', () {
+      final testJson = '''{"body": null, "header": null}''';
+      final p = Page.fromJson((jsonDecode(testJson) as Map<String, dynamic>));
+      expect(p, isA<Page>());
+    });
+
+    test('deserialise Page with Header', () {
+      final testJson = '''{"body": null, "header": { "title" : "foo" }}''';
+      final p = Page.fromJson((jsonDecode(testJson) as Map<String, dynamic>));
+      expect(p, isA<Page>());
+      expect(p.header, isA<Header>());
+      expect(p.header.title, equals('foo'));
+    });
+  });
+}
