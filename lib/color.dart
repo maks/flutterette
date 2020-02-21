dc1541d806de7bc427db60da92bcc11d857eacc9
Maksim Lin <maks@manichord.com>
2020-02-21 11:19:31 +1100

    improve analyser settings

--- lib/color.dart
+++ lib/color.dart
@@ -14,9 +14,9 @@ class HexColor extends Color {
 
   static int _parseColor(String color) {
     try {
-      color = color.toUpperCase().replaceAll("#", "");
+      color = color.toUpperCase().replaceAll('#', '');
       if (color.length == 6) {
-        color = "FF" + color;
+        color = 'FF' + color;
       }
     } on Exception catch (_) {
       return Colors.white.value;
